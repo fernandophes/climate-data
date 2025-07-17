eb57de05f019296a2884ad261db4906559e6d9a0
vitorrafaelm <vitor.rafael1518@gmail.com>
2025-07-17 20:44:47 -0300

    fix: topic publish

--- utils/src/main/java/br/edu/ufersa/cc/pd/mq/MqttConnection.java
+++ utils/src/main/java/br/edu/ufersa/cc/pd/mq/MqttConnection.java
@@ -26,171 +26,170 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MqttConnection<T> implements MqConnection<T>, MqSubscriber<T> {
 
-    private static final Logger LOG = LoggerFactory.getLogger(MqttConnection.class.getSimpleName());
+  private static final Logger LOG = LoggerFactory.getLogger(MqttConnection.class.getSimpleName());
 
-    private final MqConnectionData data;
-    private final Class<T> messageType;
-    private final String topic;
-    private final String clientId;
+  private final MqConnectionData data;
+  private final Class<T> messageType;
+  private final String topic;
+  private final String clientId;
 
-    @Getter
-    private MqttClient client;
-    private T lastReceivedMessage;
+  @Getter
+  private MqttClient client;
+  private T lastReceivedMessage;
 
-    public MqttConnection(MqConnectionData data, Class<T> messageType, String topic) {
-        this.data = data;
-        this.messageType = messageType;
-        this.topic = topic;
-        this.clientId = "mqtt-client-" + UUID.randomUUID().toString();
-    }
+  public MqttConnection(MqConnectionData data, Class<T> messageType, String topic) {
+    this.data = data;
+    this.messageType = messageType;
+    this.topic = topic;
+    this.clientId = "mqtt-client-" + UUID.randomUUID().toString();
+  }
 
-    public MqttConnection(MqttConnectionData mqttData, Class<T> messageType, String topic) {
-        this.data = new MqConnectionData(mqttData.getHost(), mqttData.getPort(),
-                mqttData.getUsername(), mqttData.getPassword());
-        this.messageType = messageType;
-        this.topic = topic;
-        this.clientId = "mqtt-client-" + UUID.randomUUID().toString(); // Generate unique client ID
-    }
+  public MqttConnection(MqttConnectionData mqttData, Class<T> messageType, String topic) {
+    this.data = new MqConnectionData(mqttData.getHost(), mqttData.getPort(),
+        mqttData.getUsername(), mqttData.getPassword());
+    this.messageType = messageType;
+    this.topic = topic;
+    this.clientId = "mqtt-client-" + UUID.randomUUID().toString(); // Generate unique client ID
+  }
 
-    @Override
-    public void createConnection() {
-        LOG.info("Connecting to MQTT broker at {}:{} with user {}", data.getHost(), data.getPort(), data.getUsername());
+  @Override
+  public void createConnection() {
+    LOG.info("Connecting to MQTT broker at {}:{} with user {}", data.getHost(), data.getPort(), data.getUsername());
 
-        try {
-            final var brokerUrl = String.format("tcp://%s:%d", data.getHost(), data.getPort());
-            client = new MqttClient(brokerUrl, clientId);
+    try {
+      final var brokerUrl = String.format("tcp://%s:%d", data.getHost(), data.getPort());
+      client = new MqttClient(brokerUrl, clientId);
 
-            final var options = new MqttConnectOptions();
-            configureConnectionOptions(options);
+      final var options = new MqttConnectOptions();
+      configureConnectionOptions(options);
 
-            client.setCallback(new MqttCallback() {
-                @Override
-                public void connectionLost(Throwable cause) {
-                    LOG.warn("MQTT connection lost", cause);
-                }
-
-                @Override
-                public void messageArrived(String topic, MqttMessage message) throws Exception {
-                    final var messageAsString = new String(message.getPayload());
-                    lastReceivedMessage = JsonUtils.fromJson(messageAsString, messageType);
-                    LOG.debug("Message received on topic {}: {}", topic, messageAsString);
-                }
-
-                @Override
-                public void deliveryComplete(IMqttDeliveryToken token) {
-                    LOG.debug("Message delivery complete: {}", token.getMessageId());
-                }
-            });
-
-            client.connect(options);
-
-            // Subscribe to the topic for receiving messages
-            client.subscribe(topic);
-
-            LOG.info("Connected to MQTT broker and subscribed to topic: {}", topic);
-
-        } catch (final MqttException e) {
-            throw new MqConnectionException("Failed to create MQTT connection", e);
-        }
-    }
-
-    @Override
-    public T receive() {
-        if (!isConnected()) {
-            throw new MqConnectionException("MQTT client is not connected");
+      client.setCallback(new MqttCallback() {
+        @Override
+        public void connectionLost(Throwable cause) {
+          LOG.warn("MQTT connection lost", cause);
         }
 
-        T message = lastReceivedMessage;
-        lastReceivedMessage = null;
-        return message;
-    }
-
-    @Override
-    public String subscribe(Consumer<T> consumer) {
-        client.setCallback(new MqttCallback() {
-            @Override
-            public void connectionLost(Throwable cause) {
-                LOG.warn("MQTT connection lost", cause);
-            }
-
-            @Override
-            public void messageArrived(String topic, MqttMessage message) throws Exception {
-                final var messageAsString = new String(message.getPayload());
-                final var object = JsonUtils.fromJson(messageAsString, messageType);
-                consumer.accept(object);
-                LOG.debug("Message received on topic {}: {}", topic, messageAsString);
-            }
-
-            @Override
-            public void deliveryComplete(IMqttDeliveryToken token) {
-                LOG.debug("Message delivery complete: {}", token.getMessageId());
-            }
-        });
-
-        return "Ok";
-    }
-
-    @Override
-    public void send(T message) {
-        if (!isConnected()) {
-            throw new MqConnectionException("MQTT client is not connected");
+        @Override
+        public void messageArrived(String topic, MqttMessage message) throws Exception {
+          final var messageAsString = new String(message.getPayload());
+          lastReceivedMessage = JsonUtils.fromJson(messageAsString, messageType);
+          LOG.debug("Message received on topic {}: {}", topic, messageAsString);
         }
 
-        try {
-            final var messageJson = JsonUtils.toJson(message);
-            final var mqttMessage = new MqttMessage(messageJson.getBytes());
-            mqttMessage.setQos(1); // At least once delivery
-
-            client.publish(topic, mqttMessage);
-            LOG.info("Message sent to topic {}: {}", topic, message);
-
-        } catch (final MqttException e) {
-            throw new MqProducerException("Failed to send MQTT message", e);
+        @Override
+        public void deliveryComplete(IMqttDeliveryToken token) {
+          LOG.debug("Message delivery complete: {}", token.getMessageId());
         }
+      });
+
+      client.connect(options);
+
+      // Subscribe to the topic for receiving messages
+      client.subscribe(topic);
+
+      LOG.info("Connected to MQTT broker and subscribed to topic: {}", topic);
+
+    } catch (final MqttException e) {
+      throw new MqConnectionException("Failed to create MQTT connection", e);
+    }
+  }
+
+  @Override
+  public T receive() {
+    if (!isConnected()) {
+      throw new MqConnectionException("MQTT client is not connected");
     }
 
-    // New method to publish to a specific topic using the same connection
-    public void sendToTopic(String targetTopic, T message) {
-        if (!isConnected()) {
-            throw new MqConnectionException("MQTT client is not connected");
-        }
+    T message = lastReceivedMessage;
+    lastReceivedMessage = null;
+    return message;
+  }
 
-        try {
-            final var messageJson = JsonUtils.toJson(message);
-            final var mqttMessage = new MqttMessage(messageJson.getBytes());
-            mqttMessage.setQos(1); // At least once delivery
+  @Override
+  public String subscribe(Consumer<T> consumer) {
+    client.setCallback(new MqttCallback() {
+      @Override
+      public void connectionLost(Throwable cause) {
+        LOG.warn("MQTT connection lost", cause);
+      }
 
-            client.publish(targetTopic, mqttMessage);
-            LOG.info("Message sent to topic {}: {}", targetTopic, message);
+      @Override
+      public void messageArrived(String topic, MqttMessage message) throws Exception {
+        final var messageAsString = new String(message.getPayload());
+        final var object = JsonUtils.fromJson(messageAsString, messageType);
+        consumer.accept(object);
+        LOG.debug("Message received on topic {}: {}", topic, messageAsString);
+      }
 
-        } catch (final MqttException e) {
-            throw new MqProducerException("Failed to send MQTT message to topic: " + targetTopic, e);
-        }
+      @Override
+      public void deliveryComplete(IMqttDeliveryToken token) {
+        LOG.debug("Message delivery complete: {}", token.getMessageId());
+      }
+    });
+
+    return "Ok";
+  }
+
+  @Override
+  public void send(T message) {
+    if (!isConnected()) {
+      throw new MqConnectionException("MQTT client is not connected");
     }
 
-    @Override
-    public void close() throws IOException {
-        try {
-            if (client != null && client.isConnected()) {
-                client.disconnect();
-                LOG.info("MQTT client disconnected");
-            }
-        } catch (final MqttException e) {
-            throw new IOException("Failed to close MQTT connection", e);
-        }
-    }
+    try {
+      final var messageJson = JsonUtils.toJson(message);
+      final var mqttMessage = new MqttMessage(messageJson.getBytes());
+      mqttMessage.setQos(1); // At least once delivery
 
-    private void configureConnectionOptions(final MqttConnectOptions options) {
-        options.setUserName(data.getUsername());
-        options.setPassword(data.getPassword().toCharArray());
-        options.setCleanSession(true);
-        options.setAutomaticReconnect(true);
-        options.setConnectionTimeout(10);
-        options.setKeepAliveInterval(20);
-    }
+      client.publish(topic, mqttMessage);
+      LOG.info("Message sent to topic {}: {}", topic, message);
 
-    private boolean isConnected() {
-        return client != null && client.isConnected();
+    } catch (final MqttException e) {
+      throw new MqProducerException("Failed to send MQTT message", e);
     }
+  }
+
+  // New method to publish to a specific topic using the same connection
+  public void sendToTopic(String targetTopic, String message) {
+    try {
+      if (!isConnected()) {
+        throw new MqConnectionException("MQTT client is not connected");
+      }
+
+      final var mqttMessage = new MqttMessage(message.getBytes());
+      mqttMessage.setQos(1); // At least once delivery
+
+      client.publish(targetTopic, mqttMessage);
+      LOG.info("Message sent to topic {}: {}", targetTopic, message);
+
+    } catch (final MqttException e) {
+      throw new MqProducerException("Failed to send MQTT message to topic: " + targetTopic, e);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    try {
+      if (client != null && client.isConnected()) {
+        client.disconnect();
+        LOG.info("MQTT client disconnected");
+      }
+    } catch (final MqttException e) {
+      throw new IOException("Failed to close MQTT connection", e);
+    }
+  }
+
+  private void configureConnectionOptions(final MqttConnectOptions options) {
+    options.setUserName(data.getUsername());
+    options.setPassword(data.getPassword().toCharArray());
+    options.setCleanSession(true);
+    options.setAutomaticReconnect(true);
+    options.setConnectionTimeout(10);
+    options.setKeepAliveInterval(20);
+  }
+
+  private boolean isConnected() {
+    return client != null && client.isConnected();
+  }
 
 }
